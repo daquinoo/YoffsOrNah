e96792af273d224fa05500a0816ef11d6e98574b
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 21:46:03 -0700

    Update NFLTrainingScrape3.py

--- NFL/NFLTrainingScrape3.py
+++ NFL/NFLTrainingScrape3.py
@@ -5,7 +5,7 @@ import time
 from scrapfly import ScrapeConfig, ScrapflyClient, ScrapeApiResponse
 
 # Database connection setup
-conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-NFL;Uid=danny1phantom;Pwd={Popp151565__};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-NFL;Uid=danny1phantom;Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = conn.cursor()
 
 # Scrapfly setup
