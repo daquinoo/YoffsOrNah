7dc2fc8d19ca22014a80e3461fffcde0f64ffc2b
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 21:05:14 -0700

    Update NFLDataScrape8.py

--- NFL/NFLDataScrape8.py
+++ NFL/NFLDataScrape8.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pyodbc
 
 # Database connection setup
-conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-base-NFL;Uid=danny1phantom;Pwd={Popp151565__};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-base-NFL;Uid=danny1phantom;Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = conn.cursor()
 
 def fetch_html(url):
