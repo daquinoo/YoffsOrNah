2671158843089e7716943e847f65e4e8de07a891
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 21:47:41 -0700

    Update NFLDataScrape6.py

--- NFL/NFLDataScrape6.py
+++ NFL/NFLDataScrape6.py
@@ -6,7 +6,7 @@ import pyodbc
 conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};'
                       'Server=tcp:yoffsornah.database.windows.net,1433;'
                       'Database=YoffsOrNah-base-NFL;Uid=danny1phantom;'
-                      'Pwd={Popp151565__};Encrypt=yes;TrustServerCertificate=no;'
+                      'Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;'
                       'Connection Timeout=30;')
 cursor = conn.cursor()
 
