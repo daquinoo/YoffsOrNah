e7425b7c8e661d8d4e26f40996956fa0d7804f3e
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 21:11:24 -0700

    Update test.py

--- MLS/test.py
+++ MLS/test.py
@@ -12,7 +12,7 @@ import time
 # Database connection setup
 conn = pyodbc.connect(
     'Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;'
-    'Database=YoffsOrNah-base-MLS;Uid=danny1phantom;Pwd={Popp151565__};Encrypt=yes;'
+    'Database=YoffsOrNah-base-MLS;Uid=danny1phantom;Pwd={pwd};Encrypt=yes;'
     'TrustServerCertificate=no;Connection Timeout=30;'
 )
 cursor = conn.cursor()
