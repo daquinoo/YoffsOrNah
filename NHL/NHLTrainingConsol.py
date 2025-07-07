13769b08d6df13bf30a0942cae32d028f907137b
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 20:54:47 -0700

    Update NHLTrainingConsol.py

--- NHL/NHLTrainingConsol.py
+++ NHL/NHLTrainingConsol.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 def update_database():
-    conn_str = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-NHL;Uid=danny1phantom;Pwd={Popp151565__};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    conn_str = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-NHL;Uid=danny1phantom;Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
     conn = pyodbc.connect(conn_str)
     cursor = conn.cursor()
 
