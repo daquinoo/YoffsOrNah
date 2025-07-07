ea393dd4781251fd1afa3f777b66fe570b1481c6
daquinoo <95264544+daquinoo@users.noreply.github.com>
2025-07-06 21:49:41 -0700

    Update MLBYoffsUd.py

--- MLB/MLBYoffsUd.py
+++ MLB/MLBYoffsUd.py
@@ -7,7 +7,7 @@ import re
 scrapfly = ScrapflyClient(key='scp-live-c8122bf4379c43f0a0ebd066f2d38b94')
 
 # Database connection setup
-conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-MLB;Uid=danny1phantom;Pwd={Popp151565__};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:yoffsornah.database.windows.net,1433;Database=YoffsOrNah-train-MLB;Uid=danny1phantom;Pwd={pwd};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = conn.cursor()
 
 def fetch_html(url):
